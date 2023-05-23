eded6552c543d508c415fe3320bc7f107d8ab927
kcyberh <kcyberh30@gmail.com>
2023-05-23 06:18:15 -0700

    simple shell\

--- main.h
+++ main.h
@@ -98,7 +98,7 @@ typedef struct r_var_list
 /* hsh_lists.c  */
 sep_list *add_sep_node_end(sep_list **head, char sep);
 void free_sep_list(sep_list **head);
-line_list *add_line_end(line_list **head, char *line);
+line_list *add_line_node_end(line_list **head, char *line);
 void free_line_list(line_list **head);
 
 /* hsh_lists2.c */
